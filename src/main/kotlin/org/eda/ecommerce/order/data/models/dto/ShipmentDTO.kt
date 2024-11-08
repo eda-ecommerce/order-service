a1c92617c7f7a7066b882d34d792a8f07b1c01f7
OliverWich <kontakt@oliver-wich.de>
2024-11-08 17:26:18 +0100

    fix: remove unused fields

--- src/main/kotlin/org/eda/ecommerce/order/data/models/dto/ShipmentDTO.kt
+++ src/main/kotlin/org/eda/ecommerce/order/data/models/dto/ShipmentDTO.kt
@@ -4,8 +4,6 @@ import java.util.*
 
 class ShipmentDTO {
 
-    lateinit var id: UUID
-
     lateinit var orderId: UUID
 
 }
--- src/main/kotlin/org/eda/ecommerce/order/data/models/dto/StockDTO.kt
+++ src/main/kotlin/org/eda/ecommerce/order/data/models/dto/StockDTO.kt
@@ -7,10 +7,6 @@ class StockDTO {
 
     lateinit var productId: UUID
 
-    var actualStock by Delegates.notNull<Int>()
-
-    var reservedStock by Delegates.notNull<Int>()
-
     var availableStock by Delegates.notNull<Int>()
 }
 
