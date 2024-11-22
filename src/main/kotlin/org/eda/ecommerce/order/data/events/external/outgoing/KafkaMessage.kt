e4d3142b1cf3449ff94a19c89b24da93e1bc1749
JannikAlx <jannik-alx@gmx.de>
2024-11-22 20:01:47 +0100

    fix: add messageId header :)

--- src/main/kotlin/org/eda/ecommerce/order/data/events/external/outgoing/KafkaMessage.kt
+++ src/main/kotlin/org/eda/ecommerce/order/data/events/external/outgoing/KafkaMessage.kt
@@ -4,6 +4,7 @@ import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata
 import org.apache.kafka.common.header.internals.RecordHeaders
 import org.eclipse.microprofile.reactive.messaging.Message
 import org.eclipse.microprofile.reactive.messaging.Metadata
+import java.util.*
 
 open class KafkaMessage<T>(operation: Operation, value: T) : Message<T> {
     private val message: Message<T> = createMessageWithMetadata(value, operation.value)
@@ -19,6 +20,7 @@ open class KafkaMessage<T>(operation: Operation, value: T) : Message<T> {
                         add("operation", operation.toByteArray())
                         add("source", "order".toByteArray())
                         add("timestamp", System.currentTimeMillis().toString().toByteArray())
+                        add("messageId", UUID.randomUUID().toString().toByteArray())
                     }).build()
             )
             return Message.of(value, metadata)
